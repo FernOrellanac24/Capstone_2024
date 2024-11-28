eb64f7f5bdc666579d2d481a917e1ca37c587754
FernOrellanac24 <fern.orellanac@gmail.com>
2024-11-27 22:18:21 -0300

    Update controller.py

--- core/controller.py
+++ core/controller.py
@@ -5,11 +5,7 @@ import mercadopago
 class Controller:
 
     def pagar(self):
-        #credenciales vendedor
-        #{"id":1145307387,"nickname":"TESTW4MEIAYK","password":"qatest8744","site_status":"active","email":"test_user_80875557@testuser.com"}
-        # Agrega credenciales
-        sdk = mercadopago.SDK("TEST-1806542489241268-061815-630c376f47340917ec1bcd617ee7c638-1143785844")
-        # datos comprador
+        
         
         # Crea un ítem en la preferencia
         preference_data = {
